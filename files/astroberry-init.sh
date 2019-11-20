79535e899fe237e20b8d73c80c2e46e242d6377e
Radek Kaczorek <rkaczorek@gmail.com>
2019-11-20 01:26:32 +0100

    Fixing password request for astroberry-init.sh

--- files/astroberry-init.sh
+++ files/astroberry-init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 INFO="<big><b>Welcome to Astroberry Server!</b></big>\n
-Astroberry Server is a ready to use system for Raspberry Pi for controlling all your astronomy equipment.\n
 You are only one step from using Astrobery Server.\n
 Review your system configuration and make necessary changes. Make sure to choose your localisation.\n
 If you skip this step you can run it anytime from <b>Menu / Preferences / Raspberry Pi Configuration</b>\n
@@ -10,8 +9,7 @@ Project home page is located at <a href=''>www.astroberry.io</a>"
 if [ -n "$(grep '^astroberry-init.sh' /etc/xdg/lxsession/LXDE-astroberry/autostart)" ]
 then
     zenity --title "Astroberry Server" --width=400 --height=300 --info --text="$INFO" --icon-name="start-here"
-    SUDO_ASKPASS="$(echo 'astroberry')"
-#    SUDO_ASKPASS="$(zenity --password --title 'Password Required')"
+    SUDO_ASKPASS="$(zenity --password --title 'Password Required')"
     if [ -n "$SUDO_ASKPASS" ]
     then
         sudo -AE rc_gui
